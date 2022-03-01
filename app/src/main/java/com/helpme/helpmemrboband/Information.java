cc157a4c13a6decb27b64bc15e54bb87dc92fa8a
sonjy2717 <sonjy2717@naver.com>
2022-03-02 03:38:27 +0900

    내 위치 버튼과 기능 구현, 검색창 추가

--- app/src/main/java/com/helpme/helpmemrboband/Information.java
+++ app/src/main/java/com/helpme/helpmemrboband/Information.java
@@ -2,8 +2,23 @@ package com.helpme.helpmemrboband;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -17,9 +32,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class Information extends AppCompatActivity {
 
-    private static final String TAG = "HELP";
+    String TAG = "HELP";
     SupportMapFragment mapFragment;
     GoogleMap map;
+    EditText searchField;
+    Button myLocation;
+    String provider;
+    double longitude;
+    double latitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +47,7 @@ public class Information extends AppCompatActivity {
         setContentView(R.layout.activity_information);
 
         mapFragment = (SupportMapFragment)getSupportFragmentManager().
-                        findFragmentById(R.id.map);
+                findFragmentById(R.id.map);
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(@NonNull GoogleMap googleMap) {
@@ -38,8 +58,7 @@ public class Information extends AppCompatActivity {
 
                 MarkerOptions markerOptions = new MarkerOptions();
                 markerOptions.position(WORLD);
-                markerOptions.title("Here i am");
-                markerOptions.snippet("우왕 지도당");
+                markerOptions.title("내 위치");
 
                 map.addMarker(markerOptions);
 
@@ -52,8 +71,7 @@ public class Information extends AppCompatActivity {
                     @Override
                     public boolean onMarkerClick(@NonNull Marker marker) {
                         Toast.makeText(getApplicationContext(),
-                                "Marker클릭 : " + marker.getTitle() + "\n" +
-                                marker.getPosition(),
+                                "내 위치",
                                 Toast.LENGTH_SHORT).show();
 
                         return false;
@@ -65,7 +83,7 @@ public class Information extends AppCompatActivity {
                     public void onInfoWindowClick(@NonNull Marker marker) {
                         Toast.makeText(getApplicationContext(),
                                 "InfoWindow클릭 : " + marker.getTitle() + "\n" +
-                                marker.getPosition(),
+                                        marker.getPosition(),
                                 Toast.LENGTH_SHORT).show();
 
                     }
@@ -79,5 +97,65 @@ public class Information extends AppCompatActivity {
         catch (Exception e) {
             e.printStackTrace();
         }
+
+
+        // 검색어 입력 영역
+        searchField = findViewById(R.id.searchField);
+        searchField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    Toast.makeText(getApplicationContext(),
+                            searchField.getText().toString(),
+                            Toast.LENGTH_SHORT).show();
+                }
+
+                return false;
+            }
+        });
+
+
+
+        //내 위치 버튼 영역
+        myLocation = findViewById(R.id.myLocation);
+        myLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= 23 &&
+                        ContextCompat.checkSelfPermission(
+                                getApplicationContext(),
+                                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(Information.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                }
+                else {
+                    Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    provider = location.getProvider();
+                    longitude = location.getLongitude();
+                    latitude = location.getLatitude();
+
+                    lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, gpsLocationListener);
+                    lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, gpsLocationListener);
+                }
+                LatLng myLocation = new LatLng(latitude, longitude);
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(myLocation, 18));
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(myLocation);
+                markerOptions.title("내 위치");
+
+                map.addMarker(markerOptions);
+            }
+        });
     }
+
+    final LocationListener gpsLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            provider = location.getProvider();
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+        }
+    };
 }
